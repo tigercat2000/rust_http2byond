118e84d17780f9ac9357a8b9ea2d29373e616023
tigercat2000 <nick.pilant2@gmail.com>
2025-07-04 12:34:10 -0700

    http2byond v0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-#![crate_name = "http2byond"]
-
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 use std::io::prelude::*;
-use std::net::{SocketAddr,TcpStream};
-use bytes::{Bytes, BytesMut, Buf, BufMut};
+use std::net::{SocketAddr, TcpStream};
 use std::time::Duration;
 
 /// This enum represents the possible return types of send_byond
@@ -14,17 +12,18 @@ pub enum ByondTopicValue {
 }
 
 /// Main (and only) function of this library.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `target` - A TCP SocketAddr of a Dream Daemon instance.
 /// * `topic` - The string you want sent to Dream Daemon. Make sure to always start this with the character `?`.ByondTopicValue
-/// 
+///
 /// # Examples
-/// 
+///
 /// ```
 /// use http2byond::{send_byond, ByondTopicValue};
-/// match send_byond(&SocketAddr::from(([127, 0, 0, 1], 1337)), "?status") {
+/// use std::net::SocketAddr;
+/// match send_byond(&SocketAddr::from(([127, 0, 0, 1], 27012)), "?status") {
 ///     Err(_) => {}
 ///     Ok(btv_result) => {
 ///         match btv_result {
@@ -58,12 +57,10 @@ pub fn send_byond(target: &SocketAddr, topic: &str) -> std::io::Result<ByondTopi
     // End with a 00
     buf.put_u8(0x0);
 
-    println!("{:02X?}", &buf[..]);
+    stream.write_all(&buf)?;
 
-    stream.write(&buf)?;
-
-    let mut recv_buf = [0; 1024];
-
+    // Receive response
+    let mut recv_buf = [0; 4096];
     let bytes_read = stream.read(&mut recv_buf)?;
 
     if bytes_read == 0 {
@@ -72,40 +69,43 @@ pub fn send_byond(target: &SocketAddr, topic: &str) -> std::io::Result<ByondTopi
 
     let mut recv_buf = Bytes::from(Vec::from(recv_buf));
 
-    if recv_buf.get_u16() == 0x0083 {
-        let mut size = recv_buf.get_u16() - 1;
-        let data_type = recv_buf.get_u8();
+    if recv_buf.try_get_u16()? == 0x0083 {
+        let mut size = recv_buf.try_get_u16()? - 1;
+        let data_type = recv_buf.try_get_u8()?;
 
         let ret = match data_type {
-            0x2a => ByondTopicValue::Number(recv_buf.get_f32_le()),
+            0x2a => ByondTopicValue::Number(recv_buf.try_get_f32_le()?),
             0x06 => {
                 let mut str = String::new();
                 while size > 0 {
-                    str.push(recv_buf.get_u8() as char);
+                    str.push(recv_buf.try_get_u8()? as char);
                     size -= 1;
                 }
                 ByondTopicValue::String(str)
-            },
+            }
             _ => ByondTopicValue::None,
         };
 
-        return Ok(ret)
+        return Ok(ret);
     }
 
     Ok(ByondTopicValue::None)
 }
 
-#[test]
-fn it_works() {
-    let res = send_byond(&SocketAddr::from(([127, 0, 0, 1], 1337)), "?status");
-    match res {
-        Err(x) => panic!("Error from send_byond {}", x),
-        Ok(wrapper) => {
-            match wrapper {
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let res = send_byond(&SocketAddr::from(([127, 0, 0, 1], 27012)), "?status");
+        match res {
+            Err(x) => panic!("Error from send_byond {x}"),
+            Ok(wrapper) => match wrapper {
                 ByondTopicValue::None => println!("Returned NONE"),
-                ByondTopicValue::String(str) => println!("Returned string {}", str),
-                ByondTopicValue::Number(num) => println!("Returned f32 {}", num)
-            }
+                ByondTopicValue::String(s) => println!("Returned string {s}"),
+                ByondTopicValue::Number(num) => println!("Returned f32 {num}"),
+            },
         }
     }
 }
